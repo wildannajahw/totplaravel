2cf46d0cfcfe7de6f028a8189a5a4440ab53852a
Alvienas Yandika <alvienasyandika@gmail.com>
2019-12-30 16:04:53 +0700

    generator

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,11 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use OTPHP\TOTP;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Initialise the 2FA class
+        $google2fa = app('pragmarx.google2fa');
+        $emailKontol;
+        $secretKontol=$google2fa->generateSecretKey();
+        $digits = 6;
+        $digest = 'sha1';
+        $period = 30;
+        $totp = TOTP::create(
+            $secretKontol, // New TOTP with custom secret
+            30,                 // The period (int)
+            'sha1',           // The digest algorithm (string)
+            6                   // The number of digits (int)
+        );
+        $totp->now();
+        $QR_Image = $google2fa->getQRCodeInline(
+            config('app.name'),
+            $emailKontol='wildan.najah@gmail.com',
+            $secretKontol
+        );
+
+        // Pass the QR barcode image to our view
+        return view('google2fa.home', ['QR_Image' => $QR_Image, 'secret' => $secretKontol, 'codeOTP' => $totp->now()]);
     }
 }
